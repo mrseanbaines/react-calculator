092a50532f335b2d19bc417a5792373df0188797
Sean Baines <mrseanbaines@gmail.com>
2018-08-17 21:49:50 +0100

    Initial setup and structure

---
+++ src/components/NumberPad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumberPad extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default NumberPad;
---
+++ src/components/Total.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Total extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Total;
